4c287e28a661a18e7500fa8b5305fd4cbaaf8388
Eduardo Guimaraes <eduguimara@gmail.com>
2021-05-18 16:54:27 -0300

    Insert method

--- backend/src/main/java/com/devsuperior/clientProject/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/clientProject/resources/ClientResource.java
@@ -4,10 +4,10 @@ import com.devsuperior.clientProject.dto.ClientDTO;
 import com.devsuperior.clientProject.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,5 +22,20 @@ public class ClientResource {
         List<ClientDTO> clients = service.findAll();
 
         return ResponseEntity.ok().body(clients);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        ClientDTO client = service.findById(id);
+
+        return ResponseEntity.ok().body(client);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+        ClientDTO client = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(client.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(client);
     }
 }
--- backend/src/main/java/com/devsuperior/clientProject/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/clientProject/services/ClientService.java
@@ -6,8 +6,10 @@ import com.devsuperior.clientProject.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -16,9 +18,38 @@ public class ClientService {
     @Autowired
     private ClientRepository repository;
 
+    @Transactional(readOnly = true)
     public List<ClientDTO> findAll() {
         List<Client> clients = repository.findAll();
 
         return clients.stream().map(client -> new ClientDTO(client)).collect(Collectors.toList());
     }
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Optional<Client> client = repository.findById(id);
+        Client entity = client.get();
+
+        return new ClientDTO(entity);
+    }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto) {
+        Client client = new Client();
+        client = copyDtoToEntity(dto, client);
+        client = repository.save(client);
+
+        return new ClientDTO(client);
+    }
+
+    private Client copyDtoToEntity(ClientDTO dto, Client entity) {
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+
+        return entity;
+    }
+
 }
